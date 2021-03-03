c12e56dd3bda4ddaa279e74005f3ff62807035bb
Jose Coelho <zecoelho2000@hotmail.com>
2021-03-03 12:08:22 +0000

    V2.0 Correction

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
-import { ChallengesProvider } from "../contexts/ChallengesContext";
 function MyApp({ Component, pageProps }) {
   return (
-       <ChallengesProvider>
           <ThemeProvider theme={theme}>
                 <Component {...pageProps} />
                 <GlobalStyle></GlobalStyle>
           </ThemeProvider>
-        </ChallengesProvider>
 
   ) 
 }
